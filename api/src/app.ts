e39422cecd3f08f6917047483386cf3932b09bf7
Wesley <wesley.brazil@outlook.com>
2023-04-22 00:25:31 -0300

    feat: configs iniciais da api

---
+++ api/src/app.ts
@@ -0,0 +1,15 @@
+import Server from '@infrastructure/server.infra';
+
+class App {
+  private readonly server: Server;
+
+  constructor() {
+    this.server = new Server();
+  }
+
+  initialize() {
+    this.server.initialize();
+  }
+}
+
+export default new App().initialize();
---
+++ api/src/infrastructure/server.infra.ts
@@ -0,0 +1,32 @@
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const PORT = process.env.PORT;
+
+class Server {
+  private readonly server: express.Application;
+  constructor() {
+    this.server = express();
+    this.initialize();
+  }
+
+  initialize() {
+    this.listen();
+  }
+
+  middlewares() {
+    this.server.use('/api', cors(), express.json(), express.urlencoded({ extended: true }));
+    this.server.use('/storage', express.static(path.join(__dirname, '..', '..', 'storage')));
+  }
+
+  controllers() {}
+  
+  listen() {
+    this.server.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
+  }
+}
+
+export default Server;
